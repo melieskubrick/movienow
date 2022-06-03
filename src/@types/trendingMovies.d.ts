fd13ca75912e34f6b4cd3de3078e50ddeedcc36b
Melies Kubrick <melies.alves@e-unicred.com.br>
2022-06-03 07:31:27 -0300

    chore: create @types to return informations

---
+++ src/@types/trendingMovies.d.ts
@@ -0,0 +1,17 @@
+declare type TrendingMovies = {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  poster_path: string
+  video: false
+  vote_average: number
+  vote_count: number
+  overview: string
+  release_date: string
+  title: string
+  popularity: number
+  media_type: string
+}
